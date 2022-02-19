805e7135e4d05975d448d5fd27a26c915a141b55
Eomy <hmu1212@hotmail.com>
2022-02-19 15:25:58 +0900

    [faet] reducers

--- index.js
+++ index.js
@@ -27,3 +27,32 @@ const createClaim = (name, amountOfMoneyToCollect) => {
     },
   };
 };
+
+// reducers
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return [...oldListOfClaims, action.payload];
+  }
+
+  return oldListOfClaims;
+};
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === "CREATE_CLAIM") {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if (action.type === "CREATE_POLICY") {
+    return bagOfMoney + action.payload.amount;
+  }
+
+  return bagOfMoney;
+};
+
+const policies = (listOfPolicies = [], action) => {
+  if (action.type === "CREATE_POLICY") {
+    return [...listOfPolicies, action.payload.name];
+  } else if (action.type === "DELETE_POLICY") {
+    return listOfPolicies.filter((name) => name !== action.payload.name);
+  }
+
+  return listOfPolicies;
+};
