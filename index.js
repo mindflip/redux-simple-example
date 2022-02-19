40b5fc47f7465f710e23fcb6ea1d895e41c0f7d7
Eomy <hmu1212@hotmail.com>
2022-02-19 15:09:00 +0900

    [feat] action creator

---
+++ index.js
@@ -0,0 +1,29 @@
+// action creator
+const createPolicy = (name, amount) => {
+  return {
+    type: "CREATE_POLICY",
+    payload: {
+      name,
+      amount,
+    },
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: "DELETE_POLICY",
+    payload: {
+      name,
+    },
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: "CREATE_CLAIM",
+    payload: {
+      name,
+      amountOfMoneyToCollect,
+    },
+  };
+};
